5d6066d20c90d1a52b210c33ffa9c8cbcc7dc4f5
pambalos <bradleyjusticeca@gmail.com>
2018-08-02 18:51:37 -0700

    Changing to Packages Structure
    Committing changes that were made to change the module design into a packages structure.

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -14,42 +14,11 @@
 #       http://localhost:5000
 #
 #    3. ctl-C on terminal to kill the server program when you are done.
-# Import Flask so that we can create an app i nstance
-from datetime import datetime
-from flask import Flask, render_template, url_for, flash, redirect
-from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm, LoginForm
-# All Flask app must create an app instance like this:
-app = Flask(__name__)
-app.config['SECRET_KEY'] =' 67aGHYDS8c7S8CGcaydw878csa7887bac' #setting secret key
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' #importing sqldatabase
+from flask import render_template, url_for, flash, redirect
+from flaskblog import app
+from flaskblog.forms import RegistrationForm, LoginForm #When in package, import with 'pkg_name.file'
+from flaskblog.models import User, Post
 
-db = SQLAlchemy(app) #database structure as models
-
-class User(db.Model): #User data base class
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(20), unique = True, nullable = False)
-    email = db.Column(db.String(20), unique = True, nullable = False)
-    image_file = db.Column(db.String(20), nullable = False, default = 'default.jpg')
-    password = db.Column(db.String(60), nullable = False)
-    posts = db.relationship('Post', backref = 'author', lazy = True)
-
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
-class Post(db.Model): #post data base class
-    id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String(120), nullable = False)
-    content = db.Column(db.Text, nullable = False)
-    date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
-
-    def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
-
-# posts = db.Post.query.all()
-
-"""
 posts = [
     {
         #dummy data
@@ -65,7 +34,7 @@ posts = [
         'date_posted' : 'April 22, 2018'
     }
 ]
-"""
+
 # Invoke this with http://127.0.0.1:5000
 @app.route('/')
 @app.route('/home')
@@ -103,8 +72,3 @@ def hello():
       f.write("This is line %d\r\n" % (i + 1))
    f.close()
    return '<h1>Hello World this is me</h1>'
-
-
-# Now, run the app as a server in debug mode
-if __name__ == '__main__':
-    app.run(debug=True)
---
+++ run.py
@@ -0,0 +1,4 @@
+from flaskblog import app
+# Now, run the app as a server in debug mode
+if __name__ == '__main__':
+    app.run(debug=True)
